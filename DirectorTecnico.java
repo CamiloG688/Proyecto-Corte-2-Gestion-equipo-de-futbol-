1eaaa1e4f8d89bb66f4f77ccc309657ab807289a
Henrry Camilo Gomez <hcgomez@uniempresarial.edu.co>
2026-09-04 18:33:06 -0500

    Creacion metodos contructore clase Director

--- DirectorTecnico.java
+++ DirectorTecnico.java
@@ -3,4 +3,20 @@ public class DirectorTecnico extends Persona{
     private int añosExperiencia = 0;
     private boolean tieneLicencia = 0;
     private double salario = 0;
+
+    public DirectorTecnico(String nombre) {
+        super(nombre);
+    }
+    public DirectorTecnico(String nombre, int edad) {
+        super(nombre, edad);
+    }
+    public DirectorTecnico(String nombre, int edad, double altura, long documento,String especialidad, int añosExperiencia,double salario, boolean tieneLicencia) {
+
+        super(nombre, edad, altura, documento);
+
+        this.especialidad = especialidad;
+        this.añosExperiencia = añosExperiencia;
+        this.salario = salario;
+        this.tieneLicencia = tieneLicencia;
+    }
 }
