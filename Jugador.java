0b73ec7bbd5c3ea9093248542e27b2b909c580f8
Henrry Camilo Gomez <hcgomez@uniempresarial.edu.co>
2026-09-04 18:28:06 -0500

    creacon de metodos constructores de la clase jugador

--- Jugador.java
+++ Jugador.java
@@ -4,5 +4,19 @@ public class Jugador extends Persona{
     private double salario = 0;
     private boolean capitan;
 
-    
+    public Jugador(String nombre, int edad) {
+        super(nombre, edad);
+    }
+    public Jugador(String nombre, int edad, double altura, long documento) {
+        super(nombre, edad, altura, documento);
+    }
+     public Jugador(String nombre, int edad, double altura, long documento,int numeroCamiseta, String posicion,double salario, boolean capitan) {
+
+        super(nombre, edad, altura, documento);
+
+        this.numeroCamisa = numeroCamiseta;
+        this.posicion = posicion;
+        this.salario = salario;
+        this.capitan = capitan;
+    }
 }
