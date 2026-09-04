a5ca29157b7fd37fc904b7db26f6c8fb8e86e481
Henrry Camilo Gomez <hcgomez@uniempresarial.edu.co>
2026-09-04 18:35:23 -0500

    creacion metodos contructores de la clase de arbitro

--- Arbitro.java
+++ Arbitro.java
@@ -3,4 +3,20 @@ public class Arbitro extends Persona{
     private int partidosDirigidos = 0;
     private double calificacion = 0;
     private boolean disponible;
+
+    public Arbitro(String nombre) {
+        super(nombre);
+    }
+    public Arbitro(String nombre, int edad) {
+        super(nombre, edad);
+    }
+    public Arbitro(String nombre, int edad, double altura, long documento,String categoria, int partidosDirigidos,double calificacion, boolean disponible) {
+
+        super(nombre, edad, altura, documento);
+
+        this.categoria = categoria;
+        this.partidosDirigidos = partidosDirigidos;
+        this.calificacion = calificacion;
+        this.disponible = disponible;
+    }
 }
